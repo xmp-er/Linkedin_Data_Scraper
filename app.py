b93c4091168762e4ef1deb52d33cb26e675d5d57
xmp-er <bankofhawkgatsu@gmail.com>
2023-02-24 21:22:12 +0530

    testing with postman

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-
+from flask import Flask,render_template,request,jsonify
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route('/',methods=['POST'])
+def m():
+    return render_template('index.html')
+
+@app.route('/company_results',methods=['POST'])
+def company_results():
+    if request.method=='POST':
+        company_name=request.form('company-name')
+        base_url='https://www.linkedin.com/search/results/all/?keywords='
+        company_linkedin_search_url=base_url+company_name
+        return company_linkedin_search_url
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
