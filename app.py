59d6b6eb6425f36d19e56539a41f8386769fcef3
xmp-er <bankofhawkgatsu@gmail.com>
2023-02-24 23:30:38 +0530

    backend made for scraping reddit data

--- app.py
+++ app.py
@@ -7,13 +7,36 @@ app = Flask(__name__)
 def m():
     return render_template('index.html')
 
-@app.route('/company_results',methods=['POST'])
+@app.route('/post_query',methods=['POST'])
 def company_results():
     if request.method=='POST':
-        company_name=request.form['company-name']
-        base_url='https://www.linkedin.com/search/results/all/?keywords='
-        company_linkedin_search_url=base_url+company_name
-        return company_linkedin_search_url
+        post_query=request.form['post_query']
+        base_url='https://www.reddit.com/search/?q='
+        post_search_url=base_url+post_query
+        #check if no results
+        post_search_page=urlopen(post_search_url)
+        post_search_page = bs(post_search_page, 'html.parser')
+        divs_containing_posts=post_search_page.findAll('div',{'class':'_2dkUkgRYbhbpU_2O2Wc5am'})
+        res=""
+        if(len(divs_containing_posts)==0):
+            res+="No posts"
+        else:
+            for i in range(len(divs_containing_posts)):
+                title_of_the_current_post=divs_containing_posts[i].find('div',{'class':'_1AKeAGcglmBjK1SUUXNFti _1-SZ3VwLjbFwTzaZvU8FBX _1FT0e6kh1BBb_oALAMW_l7 _1yBpz1MEPxxYTxjlEilGtB'}).h3.text
+                #checking if the post has comments or not
+                #the 1st element of the bar denotes the comments
+                comments_of_current_post=divs_containing_posts[i].findAll('span',{'class':'_vaFo96phV6L5Hltvwcox'})[1].text
+                final_comments=''
+                if(comments_of_current_post=='0 comments'):
+                    final_comments='No comments'
+                else:
+                    post_page=divs_containing_posts[i].div.div.find('div',{'class':'_2n04GrCyhhQf-Kshn7akmH _19FzInkloQSdrf0rh3Omen'})
+                    post_page=post_page.div.div.a['href']
+                    post_link='https://www.reddit.com/'+post_page
+                    post_upvotes=divs_containing_posts[i].findAll('span',{'class':'_vaFo96phV6L5Hltvwcox'})[0].text
+                    final_comments=divs_containing_posts[i].findAll('span',{'class':'_vaFo96phV6L5Hltvwcox'})[1].text
+                res+='{} {} {} {} {} <br>'.format(title_of_the_current_post,comments_of_current_post,post_upvotes,final_comments,post_link)
+        return res
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
